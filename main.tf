1359a5899bc25047bc2f7959fa5034937397b628
rmdhananjay <rmdhananjay.ltd@gmail.com>
2022-11-09 07:44:06 +0530

    adding workflows

--- main.tf
+++ main.tf
@@ -14,27 +14,27 @@ provider "aws" {
   region  = "ap-northeast-1"
   # if you run terraform locally then you will need to replace this with your credentials/aws-profile
   # and if you want to run through Github actions then you need to configure Secrets into Github
-  # profile = "aws-profile"
+  profile = "credentials/aws-profile"
 }
 
-data "archive_file" "lambda_terraform_test" {
+data "archive_file" "lambda_plait_demo" {
   type = "zip"
 
-  source_dir  = "${path.module}/terraform_test"
-  output_path = "${path.module}/terraform_test.zip"
+  source_dir  = "${path.module}/plait_demo"
+  output_path = "${path.module}/plait_demo.zip"
 }
 
 # to Create function
-resource "aws_lambda_function" "terraform_test" {
-  function_name = "terraform_test"
-  filename      = "terraform_test.zip"
+resource "aws_lambda_function" "plait_demo" {
+  function_name = "plait_demo"
+  filename      = "plait_demo.zip"
   runtime = "nodejs14.x"
   handler = "index.handler"
-  source_code_hash = data.archive_file.lambda_terraform_test.output_base64sha256
+  source_code_hash = data.archive_file.lambda_plait_demo.output_base64sha256
   role             = aws_iam_role.lambda_exec.arn
 }
 
-resource "aws_cloudwatch_log_group" "terraform_test" {
+resource "aws_cloudwatch_log_group" "plait_demo" {
   name = "/aws/lambda/terraform_test"
   retention_in_days = 30
 }
@@ -59,4 +59,3 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-view rawtest_terraform.tf hosted with ❤ by GitHub
