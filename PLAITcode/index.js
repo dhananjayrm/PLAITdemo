7c04d416f4a1f74376d0c55062bbbe53ce66b239
rmdhananjay <rmdhananjay.ltd@gmail.com>
2022-11-09 09:37:42 +0530

    works1

--- PLAITcode/index.js
+++ PLAITcode/index.js
@@ -1,6 +1,7 @@
 exports.handler = async (event) => {
     // TODO implement
     console.log(event);
+    //dummy
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
